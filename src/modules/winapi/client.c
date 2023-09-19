ebe6bbeff58df47c881a23653d44c6a4380437d7
MarkLagodych <lgd.mrk@gmail.com>
2023-09-19 23:07:18 +0200

    Added winapi platform (added unadded files)

---
+++ src/modules/winapi/client.c
@@ -0,0 +1,8 @@
+#include "src/modules/winapi/client.h"
+
+
+fj_err_t fj_winapi_client_run(struct fj_client * client)
+{
+    (void) client;
+    return FJ_OK;
+}
---
+++ src/modules/winapi/client.h
@@ -0,0 +1,15 @@
+#ifndef FEJIX_INTERNAL_WINAPI_CLIENT_H_
+#define FEJIX_INTERNAL_WINAPI_CLIENT_H_
+
+
+#include <fejix/client.h>
+
+#include <windows.h>
+
+
+struct fj_winapi_data {
+    HINSTANCE program_instance;
+};
+
+
+#endif
