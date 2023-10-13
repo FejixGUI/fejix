c6afb16652e7c3345c94e81a97dc75836143ca4c
MarkLagodych <lgd.mrk@gmail.com>
2023-10-13 18:20:11 +0200

    Work in progress

--- include/fejix/client.h
+++ include/fejix/client.h
@@ -5,32 +5,66 @@
 #include <fejix/base.h>
 
 
-struct fj_launch_info {
-    fj_string_t client_id;
+typedef uint32_t fj_interface_id_t;
 
-    fj_err_t (* init)(
-        void * user_data,
-        void * client_data,
-        struct fj_client_interface * client,
-        struct fj_client_feature_info * features,
-        uint32_t feature_count
+enum fj_interface_id_values {
+    FJ_CLIENT_RUN_ID = 1,
+    FJ_CLIENT_SCHEDULE_ID,
+};
+
+
+typedef uint32_t fj_launcher_flags_t;
+
+enum fj_launcher_flags_values {
+    /* `launch()` actually runs the program.
+        When it returns, the program entrypoint should return. */
+    FJ_LAUNCHER_BLOCKING = (1<<0),
+
+    /* `launch()` function only sets the launch info for the launcher.
+        After it returns, the library entrypoint should return for the launcher
+        to run. */
+    FJ_LAUNCHER_DEFERRED = (1<<1),
+};
+
+
+struct fj_client_interface_listener {
+    void (* FJ_CALLBACK init)(
+        void * FJ_NULLABLE user_data,
+        void * interface
     );
+
+    fj_interface_id_t interface_id;
+};
+
+
+struct fj_client_info {
+    void * client_data;
+    struct fj_client_interface_listener * FJ_ARRAY client_interface_listeners;
+    uint32_t client_interface_count;
+};
+
+
+struct fj_launch_info {
+    fj_err_t (* FJ_CALLBACK init)(
+        void * FJ_NULLABLE user_data,
+        struct fj_client_info const * client_info
+    );
+
+    void * FJ_NULLABLE user_data;
 };
 
 
 struct fj_launcher {
-    fj_string_t platform_name;
+    fj_err_t (* launch)(struct fj_launch_info const * info);
 
-    fj_err_t (* launch)(
-        void * user_data,
-        struct fj_launch_info * info
-    );
+    fj_string_t name;
+    fj_launcher_flags_t flags;
 };
 
 
-fj_string_t fj_get_selected_platform_name(void);
+fj_string_t fj_get_selected_launcher_name(void);
 
-struct fj_launcher const * fj_get_launchers(void);
+struct fj_launcher const * FJ_ARRAY fj_get_launchers(void);
 
 
 #endif
---
+++ include/fejix/state.h
@@ -0,0 +1,47 @@
+#ifndef FEJIX_STATE_H_
+#define FEJIX_STATE_H_
+
+
+#include <fejix/base.h>
+
+
+typedef uint32_t fj_state_id_t;
+
+typedef uint32_t fj_state_flags_t;
+
+enum fj_state_flags_values {
+    /* Initialisable means that the state can be created with a custom initial
+        value. */
+    FJ_STATE_INITABLE  = (1<<0),
+
+    /* Settable means that the state can be freely changed by the program. */
+    FJ_STATE_SETTABLE  = (1<<1),
+
+    /* Volatile means that the state can be freely changed outside the program
+        and that the program can handle its changes. */
+    FJ_STATE_VOLATILE  = (1<<2),
+};
+
+
+struct fj_state_info {
+    fj_err_t (* FJ_NULLABLE FJ_CALLBACK init)(
+        void * FJ_NULLABLE user_data
+    );
+
+    fj_err_t (* FJ_NULLABLE set)(
+        void * client,
+        void * FJ_NULLABLE value
+    );
+
+    fj_err_t (* FJ_NULLABLE FJ_CALLBACK handle_change)(
+        void * FJ_NULLABLE user_data,
+        void * FJ_NULLABLE new_value
+    );
+
+
+    fj_state_id_t id;
+    fj_state_flags_t flags;
+};
+
+
+#endif
